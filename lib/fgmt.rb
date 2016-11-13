a86e3aee8988c9402b1adb66500690f715fea7e7
fgonzales16 <francisgonzales@francis.local>
2016-11-13 18:36:32 +0800

    added repo

--- lib/fgmt.rb
+++ lib/fgmt.rb
@@ -6,6 +6,12 @@ module Fgmt
   YML_FGMT = YAML.load_file(File.join(File.dirname(__FILE__),"fgmt", "yml", "fgmt.yml"))
 
 
-  FGMT= YML_FGMT.map { |_, v| v }.flatten.compact.sort
+  FGMT = YML_FGMT.map { |_, v| v }.flatten.compact.sort
 
+
+  
+  YML_GLOBALCLIMATE = YAML.load_file(File.join(File.dirname(__FILE__),"fgmt", "yml", "globalclimate.yml"))
+
+
+  FGMT =  YML_GLOBALCLIMATE.map { |_, v| v }.flatten.compact.sort
 end
